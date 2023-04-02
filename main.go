28b9c30d89e91ca5b752285edb21b21b19817478
Alex <cryptonit63@yandex.ru>
2023-04-02 20:18:15 +0400

    Update main.go
    Signed-off-by: Alex  <cryptonit63@yandex.ru>

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello, Cryptonit!",
+			"message": "Hello, i'm Cryptonit. This dapp for asic repair center! Follow me",
 		})
 	})
 
-	router.Run("localhost:8080")
+	router.Run("https://www.cryptonit.fun/navi")
 }
