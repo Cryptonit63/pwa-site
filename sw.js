46990d7fd47236743b5498ac87a396b1f5906c55
Aleksandr <cryptonit63@yandex.ru>
2023-04-26 19:31:42 +0400

    sum

--- sw.js
+++ sw.js
@@ -1,2 +1,17 @@
 
+javascript
+if ('serviceWorker' in navigator) 
+
+{
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+      
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
